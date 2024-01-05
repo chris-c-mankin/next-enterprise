161ea81dd781df2165dd8a24ac9208f6c60cccb8
Chris Mankin <chris.mankin@groundfloordelivery.com>
2024-01-05 23:31:14 +1300

    Update layout and add item type filter to Board component

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={inter.className}>
       <body>
-        <header className="fixed h-16 w-full border-b border-b-slate-500"></header>
+        <header className="fixed h-16 w-full bg-zinc-900 border-b border-b-slate-500"></header>
         <section className="h-16"> </section>
         {children}
       </body>
--- components/Board/Board.tsx
+++ components/Board/Board.tsx
@@ -1,9 +1,12 @@
+"use client"
+import { useMemo, useState } from "react"
 import { FaClipboard } from "react-icons/fa"
 import { SpawnPrepTaskDto } from "../../app/api/prepItems/[id]/spawnPrepTask/route"
 import { PrepBoardDto, UserDto } from "../../mocks/mocks.interfaces"
 import { NewTabLink } from "../NewTabLink/NewTabLink"
 import { PageTitle } from "../PageTitle/PageTitle"
 import { PrepItemTileSmall } from "../PrepItemTileSmall/PrepItemTileSmall"
+import { SelectInput } from "../SelectInput/SelectInput"
 
 interface BoardProps {
   prepBoard: PrepBoardDto
@@ -15,9 +18,36 @@ interface BoardProps {
   }
 }
 
+type SelectOption = { value: string; label: string }
+const itemTypeOptions = [
+  {
+    value: "all",
+    label: "All",
+  },
+  {
+    value: "tasks",
+    label: "Tasks",
+  },
+  {
+    value: "items",
+    label: "Items",
+  },
+]
+
 export function Board({ prepBoard, userid, handlers, users }: BoardProps) {
   const prepItems = prepBoard.prepItems
   const categories = [...new Set(prepItems.map((item) => item.category))]
+
+  const [itemTypeFilter, setItemTypeFilter] = useState<SelectOption>({
+    value: "all",
+    label: "All",
+  })
+
+  const prepItemsToDisplay = useMemo(() => {
+    if (itemTypeFilter.value === "tasks") return prepItems.filter((item) => prepBoard.prepTasks.some((task) => task.prepItem.id === item.id))
+    if (itemTypeFilter.value === "items") return prepItems.filter((item) => !prepBoard.prepTasks.some((task) => task.prepItem.id === item.id))
+    return prepItems
+  }, [itemTypeFilter.value, prepBoard.prepTasks, prepItems])
 
   return (
     <>
@@ -29,12 +59,20 @@ export function Board({ prepBoard, userid, handlers, users }: BoardProps) {
       </header>
       <PageTitle title={prepBoard.name} />
       <h2 className="mb-12">{prepBoard.description}</h2>
+      <div className="mb-8">
+        <SelectInput
+          label="Item Type"
+          options={itemTypeOptions}
+          onChange={(value) => setItemTypeFilter(itemTypeOptions.find((option) => option.value === value)!)}
+          selected={itemTypeFilter}
+        />
+      </div>
       <div className="grid w-full gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {categories.map((category) => (
           <div className="rounded border border-slate-500 p-4" key={category}>
             <div className="mb-2 text-lg">{category}</div>
             <ul>
-              {prepItems
+              {prepItemsToDisplay
                 .filter((item) => item.category === category)
                 .map((item) => (
                   <li key={item.id}>
